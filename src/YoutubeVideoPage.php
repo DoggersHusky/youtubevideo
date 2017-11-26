cec0a815b04660a8186c26e6066bbd40d2c99f2c
Buckles <doghusky1990@gmail.com>
2017-11-26 19:38:51 -0400

    removed controller from page file

--- src/YoutubeVideoPage.php
+++ src/YoutubeVideoPage.php
@@ -20,31 +20,3 @@ class YoutubeVideoPage extends Page {
     }
     
 }
-
-class YoutubeVideoPage_Controller extends Page_Controller {
-
-    private static $allowed_actions = array (
-        'showVideo'
-    );
-    
-    
-    public function init() {
-        parent::init();
-        
-        Requirements::css(YOUTUBEVIDEO_BASE.'/css/styles.css');
-        
-    }
-    
-    public function showVideo(SS_HTTPRequest $request) {
-        
-        $ID = $request->param('ID');
-
-        $data = YoutubeVideo::get()->filter(array(
-            'ID' => $ID
-        ));
-        
-        return array (
-                'Data' => $data
-        );
-    }
-}
